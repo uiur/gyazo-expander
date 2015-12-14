c00ce3aaf93ba0fe034ca4a8701b19813a10ec1f
Kazato Sugimoto <uiureo@gmail.com>
2015-12-14 12:14:37 +0900

    Add quick prototype

--- src/content.js
+++ src/content.js
@@ -1,1 +1,18 @@
-console.log('hello')
+const url = require('url')
+setTimeout(() => {
+  Array.from(document.querySelectorAll('a')).forEach((el) => {
+    const href = el.getAttribute('href')
+    let parsedUrl
+
+    try {
+      parsedUrl = url.parse(href)
+    } catch (e) {
+      return
+    }
+
+    if (parsedUrl.host === 'gyazo.com' && (/^\/[0-9a-f]+$/).test(parsedUrl.path)) {
+      const imageUrl = href + '.png'
+      el.insertAdjacentHTML('afterend', `<img src=${ imageUrl } />`)
+    }
+  })
+}, 5000)
